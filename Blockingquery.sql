4e4c69b13861e74eead8a00556152d7e1b7549dd
saranrajS-awaninfotech <saranraj.sivakumar@awaninfotech.com>
2026-06-09 13:50:42 +0530

    Delete Blockingquery.sql

--- Blockingquery.sql
+++
@@ -1,6 +0,0 @@
-select * from WhatIsGoingOn -- what process is blocking
-select * from sys.sysprocesses where spid =2706 -- info abt specfic process
-dbcc inputbuffer(2177) -- to know about the statement and statement ( sleep or active )
-kill 2385
-
-
